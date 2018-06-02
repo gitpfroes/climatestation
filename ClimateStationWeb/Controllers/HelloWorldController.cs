887e47127c9642f91af06bdd537379abe9125c08
gitpfroes <paola.froes@gmail.com>
2018-06-02 19:15:21 -0300

    Adicionar arquivos de projeto.

---
+++ ClimateStationWeb/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using ClimateStationWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text.Encodings.Web;
+
+namespace ClimateStationWeb.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        static string URL = "http://date.jsontest.com/";
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            WebClient client = new WebClient();
+            string jsonData = client.DownloadString(URL);
+            Data dataAtual = JsonConvert.DeserializeObject<Data>(jsonData);
+            return "Data e hora atual: " + dataAtual.data + dataAtual.time;
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ ClimateStationWeb/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClimateStationWeb.Models
+{
+    public class Data
+    {
+        public string time { get; set; }
+        [JsonProperty("milliseconds_since_epoch")]
+        public long miliseconds { get; set; }
+        [JsonProperty("date")]
+        public string data { get; set; }
+    }
+}
